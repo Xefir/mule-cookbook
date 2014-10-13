828dec28d072554c4c3515c292561260e81f3cdc
Daniel van den Berg <dan@shopkeep.com>
2014-10-13 00:04:21 -0400

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'mule'
+maintainer       'ShopKeep DevOps'
+maintainer_email 'devops@shopkeep.com'
+license          'All rights reserved'
+description      'Installs/Configures mule'
+long_description 'Installs/Configures mule'
+version          '0.1.0'
+
+depends 'java', '~> 1.28.0'
