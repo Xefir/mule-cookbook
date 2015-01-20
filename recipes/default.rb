083ed9526aeb9f4074833a3c1640155d7f18975e
Daniel van den Berg <dan@shopkeep.com>
2015-01-20 10:24:08 -0500

    Set the uid and gid of the mule user to fixed value

--- recipes/default.rb
+++ recipes/default.rb
@@ -9,11 +9,17 @@
 
 include_recipe 'mule::_java'
 
+group node['mule']['group'] do
+  gid node['mule']['gid']
+end
+
 user node['mule']['user'] do
   supports manage_home: true
   shell '/bin/bash'
   home "/home/#{node['mule']['user']}"
   comment 'Mule user'
+  uid node['mule']['uid']
+  gid node['mule']['gid']
 end
 
 archive 'mule' do
