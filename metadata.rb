fea9c9a803c559f5dba87a66b9d49e2844655bd1
Daniel van den Berg <dan@shopkeep.com>
2015-01-20 12:57:06 -0500

    version 0.1.1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'devops@shopkeep.com'
 license          'All rights reserved'
 description      'Installs/Configures mule'
 long_description 'Installs/Configures mule'
-version          '0.1.0'
+version          '0.1.1'
 
 depends 'apt', '~> 2.6.0'
 depends 'archive', '~> 0.2.6'
